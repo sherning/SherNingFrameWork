e25d4c4b56f1b6fdc5b3d19240eaa9fc20f03950
Sher Ning <sherning@hotmail.com>
2020-11-11 14:43:36 +0800

    Update Probability Power

--- SherNingFrameWork/Probability.cs
+++ SherNingFrameWork/Probability.cs
@@ -43,6 +43,8 @@ namespace SherNingFrameWork
             int r = 7;
             Console.WriteLine("Permutation: " + Permutation(n, r));
             Console.WriteLine("Combination: " + Combination(n, r));
+            Console.WriteLine("Math Power: " + Math.Pow(3, -4));
+            Console.WriteLine("Power: " + Power(3, -4));
         }
 
         private static double Factorial(int n)
@@ -51,6 +53,8 @@ namespace SherNingFrameWork
             if (n == 0) return 1;
 
             double ret = 1;
+
+            // 3! = 3 X 2 X 1
             for (int i = n; i > 0; i--)
                 ret *= i;
 
@@ -71,6 +75,30 @@ namespace SherNingFrameWork
 
             // permutation / nPn == (n!) 
             return Permutation(n, r) / Factorial(r);
+        }
+
+        private static double Power(int num, int exponent)
+        {
+            // 0^0, defined to be 1, and not exception
+            if (num == 0 && exponent == 0) return 1;
+            if (num == 0) return 0;
+
+            // a^0 = 1, a^b x a^c = a^(b+c)
+            double ret = 1;
+
+            if (exponent >= 0)
+            {
+                for (int i = 0; i < exponent; i++)
+                    ret *= num;
+            }
+            else
+            {
+                // -exponent = abs(exponent)
+                for (int i = 0; i < -exponent; i++)
+                    ret /= num;
+            }
+
+            return ret;
         }
     }
 }
